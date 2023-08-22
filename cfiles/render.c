23176473c855f703958696b5c2dc9de026f1c539
Anthony Ramon <rhony@anthonys-mbp.home>
2023-08-22 18:51:59 +0200

    Mlx tests

--- cfiles/render.c
+++ cfiles/render.c
@@ -6,15 +6,86 @@
 /*   By: aramon <aramon@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 18:26:16 by aramon            #+#    #+#             */
-/*   Updated: 2023/08/22 18:34:50 by aramon           ###   ########.fr       */
+/*   Updated: 2023/08/22 18:51:31 by aramon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-int render(t_vars *vars)
+#define GREEN_PIXEL 0xFF00
+#define RED_PIXEL 0xFF0000
+#define WHITE_PIXEL 0xFFFFFF
+
+typedef struct s_rect
 {
-    if (vars->win != NULL)
-        mlx_pixel_put(vars->mlx, vars->win, 100, 100, 0x00FF0000);
-    return (0);
+    int	x;
+    int	y;
+    int width;
+    int height;
+    int color;
+}	t_rect;
+
+void	img_pix_put(t_data *img, int x, int y, int color)
+{
+	char    *pixel;
+	int		i;
+
+	i = img->bits_per_pixel - 8;
+    pixel = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	while (i >= 0)
+	{
+		/* big endian, MSB is the leftmost bit */
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		/* little endian, LSB is the leftmost bit */
+		else
+			*pixel++ = (color >> (img->bits_per_pixel - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
+void	render_background(t_data *img, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < WINY)
+	{
+		j = 0;
+		while (j < WINX)
+		{
+			img_pix_put(img, j++, i, color);
+		}
+		++i;
+	}
+}
+
+int render_rect(t_data *img, t_rect rect)
+{
+	int	i;
+	int j;
+
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			img_pix_put(img, j++, i, rect.color);
+		++i;
+	}
+	return (0);
+}
+
+int	render(t_vars *vars)
+{
+	if (vars->win == NULL)
+		return (1);
+	render_background(&vars->img, WHITE_PIXEL);
+	render_rect(&vars->img, (t_rect){WINX - 100, WINY - 100, 100, 100, GREEN_PIXEL});
+	render_rect(&vars->img, (t_rect){0, 0, 100, 100, RED_PIXEL});
+
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->img.img, 0, 0);
+
+	return (0);
 }
