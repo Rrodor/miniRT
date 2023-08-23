bbb34353dbf6a38b1b9d33192253f939de509197
Anthony Ramon <rhony@anthonys-mbp.home>
2023-08-23 13:33:52 +0200

    Issues on color

--- cfiles/color.c
+++ cfiles/color.c
@@ -6,13 +6,27 @@
 /*   By: aramon <aramon@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 21:11:23 by aramon            #+#    #+#             */
-/*   Updated: 2023/08/23 13:27:33 by aramon           ###   ########.fr       */
+/*   Updated: 2023/08/23 13:33:22 by aramon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "color.h"
 #include "position.h"
 #include "ray.h"
+#include <stdlib.h>
+
+t_rgb   *init_color(int r, int g, int b)
+{
+    t_rgb   *color;
+
+    color = (t_rgb *)malloc(sizeof(t_rgb));
+    if (!color)
+        return (NULL);
+    color->r = r;
+    color->g = g;
+    color->b = b;
+    return (color);
+}
 
 t_rgb    *get_color(t_ray *ray)
 {
--- hfiles/color.h
+++ hfiles/color.h
@@ -6,7 +6,7 @@
 /*   By: aramon <aramon@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 21:11:50 by aramon            #+#    #+#             */
-/*   Updated: 2023/08/23 13:28:23 by aramon           ###   ########.fr       */
+/*   Updated: 2023/08/23 13:33:25 by aramon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ typedef struct	s_rgb
     int			b;
 }				t_rgb;
 
+t_rgb           *init_color(int r, int g, int b);
 t_rgb           *ray_color(t_ray *ray);
 int             encode_rgb(t_rgb *rgb);
 
