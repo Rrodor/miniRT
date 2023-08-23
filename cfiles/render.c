4b8957816e04d427ada3c8994979fa321b2f4208
Anthony Ramon <rhony@anthonys-mbp.home>
2023-08-23 12:31:33 +0200

    Solving camera ray issues

--- cfiles/render.c
+++ cfiles/render.c
@@ -6,11 +6,13 @@
 /*   By: aramon <aramon@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 18:26:16 by aramon            #+#    #+#             */
-/*   Updated: 2023/08/23 11:12:56 by aramon           ###   ########.fr       */
+/*   Updated: 2023/08/23 12:29:40 by aramon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+#include "position.h"
+#include "ray.h"
 
 void	img_pix_put(t_data *img, int x, int y, int color)
 {
@@ -37,7 +39,7 @@ int	render(t_vars *vars)
         while (j < WINX)
         {
             pixel_center = get_pixel_center(i, j);
-            ray_dir = vec_sub(pixel_center, new_point(0, 0, 0));
+            ray_dir = vec_new(pixel_center->x, pixel_center->y, pixel_center->z); // <<-- (1) See below
             ray = new_ray(new_point(0, 0, 0), ray_dir);
             img_pix_put(&vars->img, i, j, ray_color(ray));
             j++;
@@ -47,3 +49,9 @@ int	render(t_vars *vars)
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img.img, 0, 0);
 	return (0);
 }
+
+// (1)
+// The ray direction is computed by substracting the camera_center from the pixel_center
+// Since the camera_center is always at (0, 0, 0), we can just pass the pixel_center as the ray direction
+// If the camera_center was not at (0, 0, 0), we would have to substract the camera_center from the pixel_center
+// ray_dir = vec_new(pixel_center->x - camera_center->x, pixel_center->y - camera_center->y, pixel_center->z - camera_center->z);
