87083bae5bb7dc257d7ea74d328864818e5e8ee2
Anthony Ramon <rhony@anthonys-mbp.home>
2023-08-23 13:35:06 +0200

    Issues on color

--- cfiles/color.c
+++ cfiles/color.c
@@ -6,7 +6,7 @@
 /*   By: aramon <aramon@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 21:11:23 by aramon            #+#    #+#             */
-/*   Updated: 2023/08/23 13:33:22 by aramon           ###   ########.fr       */
+/*   Updated: 2023/08/23 13:34:47 by aramon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,9 @@ t_rgb   *init_color(int r, int g, int b)
 
 t_rgb    *get_color(t_ray *ray)
 {
-    t_rgb   *color;
     t_pos   *point;
 
-    point = new_point(0, 0, -1);
-    color = init_color(0, 0, 0);
+    point = point_new(0, 0, -1);
     if (hit_sphere(point, 0.5, ray))
         return (init_color(255, 0, 0)); // red
     else
